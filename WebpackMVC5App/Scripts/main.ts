6db38e861c77c9f7f973fd9448e3d727d6ac872d
jktravis <joshua.travis@gmail.com>
2017-08-17 12:38:24 -0400

    Added global variable so that the modules can access.
    This was needed since the original bundle was funciton scoped inside of
    webpack, which basically prevented any leakage in the global/window
    scope, which was how it was being accessed.  Also separated out the Main
    class to its own file and added an App.ts to act as the bootstrap. This
    file can be renamed for the specific page or group of pages that share
    the same functionality.  Any other "groups" will need their own entry
    file.

--- WebpackMVC5App/Scripts/main.ts
+++ WebpackMVC5App/Scripts/main.ts
@@ -1,13 +1,4 @@
-﻿import * as $ from 'jquery';
-import * as bootstrap from 'bootstrap';
-import '../node_modules/@progress/kendo-ui/js/kendo.web';
-import '../node_modules/@progress/kendo-ui/js/kendo.aspnetmvc';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap/dist/css/bootstrap-theme.css';
-import '../node_modules/font-awesome/css/font-awesome.css';
-import '../node_modules/@progress/kendo-ui/css/web/kendo.common.css';
-
-export  class Main {
+﻿export default class Main {
     private _name = '';
 
     constructor(name: string) {
